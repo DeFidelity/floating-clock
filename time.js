0f6ad9ce80bcb0014401f6dd31fae5d0f5c62c41
Amusat Haki Adeyemi <adeyemihaki@gmail.com>
2022-05-29 12:30:58 +0100

    Update time.js

--- time.js
+++ time.js
@@ -3,7 +3,6 @@ let minuterCl = document.querySelector(".min")
 let secondsCl = document.querySelector(".sec")
 let periodCl = document.querySelector(".period")
 
-let date = document.querySelector(".date")
 
 function getTime() {
     var date = new Date();
@@ -26,7 +25,6 @@ function getTime() {
     minuterCl.innerHTML = min;
     secondsCl.innerHTML = sec;
     periodCl.innerHTML = period;
-    date.innerHTML = date;
 }
 
 getTime()
